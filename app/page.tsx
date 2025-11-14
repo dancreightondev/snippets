0ac84729be9bf78c9f475630ece6d533871cc08a
dancreightondev <22957063+dancreightondev@users.noreply.github.com>
2025-11-14 14:57:14 +0000

    Update global background & foreground colours

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
-			<h1 className="text-2xl font-sans text-black dark:text-zinc-50">
-				Placeholder Page
-			</h1>
+		<main className="flex min-h-screen items-center justify-center">
+			<h1 className="text-2xl font-sans">Placeholder Page</h1>
 		</main>
 	);
 }
