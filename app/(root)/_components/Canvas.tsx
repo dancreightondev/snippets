52486bc6b06096c1e6a0dedaf0d1721a792738a7
dancreightondev <22957063+dancreightondev@users.noreply.github.com>
2025-11-15 18:16:05 +0000

    Refactor `Canvas` component to improve drag-and-drop UX

--- app/(root)/_components/Canvas.tsx
+++ app/(root)/_components/Canvas.tsx
@@ -8,34 +8,46 @@ import { twClassMerge } from "~/app/utils/tailwind";
 
 interface CanvasProps extends React.HTMLAttributes<HTMLDivElement> {
 	// Custom props go here
-	onSnippetDrop?: () => void;
+	snippets?: React.ReactElement[];
+	onSnippetDrop?: (snippetId: string | null) => void;
 }
 
 export const Canvas: FC<CanvasProps> = ({
 	className,
+	snippets,
 	onSnippetDrop,
 	...props
 }) => {
-	const { setNodeRef, isOver } = useDroppable({
-		id: "canvas",
-	});
+	const { setNodeRef, isOver } = useDroppable({ id: "canvas" });
+
 	return (
-		<div
-			className={twClassMerge(
-				className,
-				"min-h-[300px] rounded-xl border-2 transition flex items-center justify-center",
-				isOver
-					? "border-primary bg-primary/10"
-					: "border-foreground bg-background"
-			)}
-			ref={setNodeRef}
-			{...props}
-		>
-			{isOver ? (
-				<span className="text-primary font-semibold">Drop here!</span>
-			) : (
-				<span className="text-foreground/50">Drag a snippet here</span>
-			)}
+		<div id="canvas" className={twClassMerge(className)}>
+			<div id="canvas-snippets" className="flex flex-col w-full">
+				{snippets}
+			</div>
+			<div
+				id="canvas-dropzone"
+				className={twClassMerge(
+					"min-h-[100px] rounded-xl transition flex items-center justify-center",
+					isOver
+						? "border-primary border-3 border-dashed bg-primary/25"
+						: ""
+				)}
+				ref={setNodeRef}
+				{...props}
+			>
+				{isOver ? (
+					<span className="text-primary font-semibold">
+						Drop it here!
+					</span>
+				) : (
+					<span className="text-midground">
+						{snippets && snippets.length < 1
+							? "Drag your first snippet here"
+							: "Drag another snippet here"}
+					</span>
+				)}
+			</div>
 		</div>
 	);
 };
