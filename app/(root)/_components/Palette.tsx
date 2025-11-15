5227a4a3ab9f7fa8889cc9aa91964d38122e1f76
dancreightondev <22957063+dancreightondev@users.noreply.github.com>
2025-11-15 18:16:23 +0000

    Add `Palette` component for rendering draggable snippets

---
+++ app/(root)/_components/Palette.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import { FC } from "react";
+import { twClassMerge } from "~/app/utils/tailwind";
+import { renderSnippet } from "../_utils/snippets";
+import { ALL_PALETTE_SNIPPETS } from "~/app/lib/constants";
+
+interface PaletteProps extends React.HTMLAttributes<HTMLDivElement> {
+	// Custom props go here
+}
+
+export const Palette: FC<PaletteProps> = ({ className, ...props }) => {
+	return (
+		<div
+			className={twClassMerge(
+				className,
+				"w-96 mx-auto flex justify-center items-center"
+			)}
+			{...props}
+		>
+			{ALL_PALETTE_SNIPPETS.map((snippet) =>
+				renderSnippet(snippet, true)
+			)}
+		</div>
+	);
+};
