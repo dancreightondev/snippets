c86a6400383f698618178136eceabaeaa635df33
dancreightondev <22957063+dancreightondev@users.noreply.github.com>
2025-11-15 18:14:49 +0000

    Enhance `BaseSnippet` to support separate palette styling

--- app/(root)/_components/BaseSnippet.tsx
+++ app/(root)/_components/BaseSnippet.tsx
@@ -1,38 +1,68 @@
 "use client";
 
-import { FC } from "react";
+import Image from "next/image";
+import { FC, isValidElement } from "react";
 import { twClassMerge } from "~/app/utils/tailwind";
+import { SnippetIcon } from "~/app/lib/types";
 import { useDraggable } from "@dnd-kit/core";
+import { BsFillQuestionCircleFill } from "react-icons/bs";
 
-interface BaseSnippetProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface BaseSnippetProps extends React.HTMLAttributes<HTMLDivElement> {
 	// Custom props go here
 	name: string;
+	icon?: SnippetIcon;
+	paletted?: boolean;
 }
 
 export const BaseSnippet: FC<BaseSnippetProps> = ({
 	name,
+	icon,
+	paletted,
 	children,
 	className,
+	id,
 	...props
 }) => {
 	const { attributes, listeners, setNodeRef, isDragging } = useDraggable({
-		id: name,
+		id: id ?? name,
 	});
-
+	if (paletted) {
+		return (
+			<div
+				className={twClassMerge(
+					className,
+					"p-4 rounded-lg shadow transition duration-60 size-min bg-primary/10 hover:bg-primary/50 hover:text-primary hover:cursor-grab",
+					isDragging ? "opacity-0 cursor-grabbing" : ""
+				)}
+				ref={setNodeRef}
+				{...listeners}
+				{...attributes}
+				{...props}
+			>
+				<span
+					id="palette-icon"
+					className="size-8 relative flex items-center justify-center text-2xl"
+				>
+					{typeof icon === "string" ? (
+						<Image
+							src={icon}
+							alt={name}
+							fill
+							className="object-contain"
+						/>
+					) : isValidElement(icon) ? (
+						<span className="size-full flex items-center justify-center">
+							{icon}
+						</span>
+					) : (
+						<BsFillQuestionCircleFill className="size-full" />
+					)}
+				</span>
+			</div>
+		);
+	}
 	return (
-		<div
-			className={twClassMerge(
-				className,
-				"hover:cursor-grab p-4 rounded-lg shadow transition",
-				isDragging
-					? "ring-2 ring-primary scale-105 bg-primary/10"
-					: "bg-background"
-			)}
-			ref={setNodeRef}
-			{...listeners}
-			{...attributes}
-			{...props}
-		>
+		<div>
 			<span className="font-bold mb-2 block">{name}</span>
 			{children}
 		</div>
